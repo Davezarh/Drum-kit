bc278ad90f958b51be111ba85411e5821021c677
Dawid <dharendarz2@gmail.com>
2021-02-20 21:12:49 +0100

    Added function remove Transition which, after sounds ends transition disappear

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,15 @@
-window.addEventListener('keydown',(e)=>{
+const playSound = (e) =>{
+    const key = document.querySelector(`.section__key[data-key="${e.keyCode}"]`) 
     const audio = document.querySelector(`audio[class="${e.keyCode}"]`)
+    key.classList.add("section__key--play")
     audio.play()
-})
+    audio.currentTime = 0; 
+}
+function removeTransition(e){
+    if(e.propertyName !== "transform") return
+    this.classList.remove("section__key--play");
+}
+const keys = document.querySelectorAll(".section__key")
+keys.forEach(key => key.addEventListener("transitionend",removeTransition))
+
+window.addEventListener('keydown',playSound)
